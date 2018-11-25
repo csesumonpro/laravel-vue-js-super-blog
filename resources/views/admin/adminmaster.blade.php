a1d675ae765c783f2fcc9b147b4ae7a3cc66e7cb
Sumon Sarker <csesumonpro@gmail.com>
2018-11-25 20:05:38 +0600

    Post Table and Category table added

--- resources/views/admin/adminmaster.blade.php
+++ resources/views/admin/adminmaster.blade.php
@@ -55,32 +55,21 @@
                                 <i class="right fa fa-angle-left"></i>
                             </p>
                         </a>
-                        <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="../../index.html" class="nav-link">
-                                    <i class="fa fa-circle-o nav-icon"></i>
-                                    <p>Dashboard v1</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="../../index2.html" class="nav-link">
-                                    <i class="fa fa-circle-o nav-icon"></i>
-                                    <p>Dashboard v2</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="../../index3.html" class="nav-link">
-                                    <i class="fa fa-circle-o nav-icon"></i>
-                                    <p>Dashboard v3</p>
-                                </a>
-                            </li>
-                        </ul>
                     </li>
                     <li class="nav-item">
                         <a href="../widgets.html" class="nav-link">
                             <i class="nav-icon fa fa-th"></i>
                             <p>
-                                Widgets
+                                Category
+
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="../widgets.html" class="nav-link">
+                            <i class="nav-icon fa fa-th"></i>
+                            <p>
+                                Post
                                 <span class="right badge badge-danger">New</span>
                             </p>
                         </a>
