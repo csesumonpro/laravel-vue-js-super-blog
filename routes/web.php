c82b41f860604403abedd3d5bc9155af44254fe8
Sumon Sarker <csesumonpro@gmail.com>
2018-11-25 18:29:45 +0600

    admin lte added

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
